78ffd80c91b3934ce6774c9660d75f2fd7637d2f
Richetti123 <148177322+Richetti123@users.noreply.github.com>
2024-10-30 02:58:03 +0100

    Update grupo-tagall.js

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,16 +4,14 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `ღ ${lenguajeGB['smsAddB5']()} ${pesan}`
-let teks = `╭━〔 *${lenguajeGB['smstagaa']()}* 〕━⬣\n\n${oi}\n\n`
+let oi = `ＭｉｃｈｉＢｏｔ ${pesan}`
+let teks = `*𝘼𝙘𝙩𝙞𝙫𝙖𝙩𝙚 𝙥𝙡𝙖𝙣𝙩𝙖 𝙙𝙚 𝙢𝙧𝙙𝙖🌱🫦*\n${oi}\n\n*＠ｃｅｏｇｅｒｉｐｉｕｍ．ｄｚｎ🫶🏻*\n`
 for (let mem of participants) {
-teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
-teks += `┃\n`
-teks += `┃ ${wm}\n`
-teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
-conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
+teks += `🍔🍟ᝰ.ᐟ @${mem.id.split('@')[0]}\n`}
+teks += `MichiBot`
+conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) } )
 }
-handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
+handler.command = /^(tagall|invocar|invocacion|todos|invocación|michi)$/i
 handler.admin = true
 handler.group = true
 handler.botAdmin = true
